c260deabe7669772ca7de95416e7fb78404f9b50
Stefano Baghino <stefano@baghino.me>
2023-03-04 10:47:26 +0100

    chore: start from scratch, be puzzled by undefined behavior

--- playground.zig
+++ playground.zig
@@ -2,10 +2,15 @@ const std = @import("std");
 const testing = std.testing;
 const expect = testing.expect;
 
-test "basic test" {
-    try expect(47 == 42 + 5);
-}
-
-test "while loop" {
-    try expect(42 == 42);
+test "assignment" {
+    const constant = 5;
+    try expect(constant == 5);
+    var variable: u8 = 5;
+    try expect(variable == 5);
+    variable = 6;
+    try expect(variable == 6);
+    const bigger_variable = @as(u32, variable);
+    try expect(bigger_variable == 6);
+    const undefined_constant: u8 = undefined;
+    try expect(undefined_constant == 6); // TODO check if this is expected
 }
